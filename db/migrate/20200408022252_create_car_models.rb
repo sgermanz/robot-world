256ad198326cee3d26f1a9fdfdba9dc624e6f1cf
Germán <german@exo.com.ar>
2020-04-08 00:03:53 -0300

    Added postgres configuration

---
+++ db/migrate/20200408022252_create_car_models.rb
@@ -0,0 +1,12 @@
+class CreateCarModels < ActiveRecord::Migration[6.0]
+  def change
+    create_table :car_models do |t|
+      t.string :name
+      t.decimal :cost
+      t.decimal :price
+      t.integer :minStock
+
+      t.timestamps
+    end
+  end
+end
