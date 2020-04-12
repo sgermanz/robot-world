abe535e8a509b6a6336bb9f6dcc44dd2bb7a81f8
Germán <german@exo.com.ar>
2020-04-12 17:30:37 -0300

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,60 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-carModels = CarModel.create(
-    [
-        { 
-            name: "Phoebe",
-            cost: 100000,
-            price: 200000
-        }, 
-        { 
-            name: "Ganymede",
-            cost: 200000,
-            price: 400000
-        }, 
-        { 
-            name: "Callisto",
-            cost: 300000,
-            price: 600000
-        }, 
-        { 
-            name: "Deimos",
-            cost: 400000,
-            price: 800000
-        }, 
-        { 
-            name: "Titan",
-            cost: 500000,
-            price: 100000
-        }, 
-        { 
-            name: "Europa",
-            cost: 600000,
-            price: 1200000
-        }, 
-        { 
-            name: "Io",
-            cost: 700000,
-            price: 1400000
-        }, 
-        { 
-            name: "Hyperion",
-            cost: 800000,
-            price: 1600000
-        }, 
-        { 
-            name: "Rhea",
-            cost: 900000,
-            price: 1800000
-        }, 
-        { 
-            name: "Phobos",
-            cost: 1000000,
-            price: 2000000
-        }
-    ]
-)
+modelNames = ["Phoebe", "Ganymede", "Callisto", "Deimos", "Titan", "Europa", "Io", "Hyperion", "Rhea", "Phobos"]
+models = Array.new
+modelNames.each do |name|
+    model = Hash.new
+    model[:name] = name
+    model[:cost] = (100000..1000000).to_a.sample
+    model[:price] = model[:cost] * 2
+    models.push(model)
+end
+carModels = CarModel.create(models)
 
 storeConfiguration = {
     buyer_schedule: 10,
