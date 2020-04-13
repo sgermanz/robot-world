b5290c4d35bdc2dbfc969c78baf75a4c31f4602b
Germán <german@exo.com.ar>
2020-04-13 07:11:24 -0300

    guard tests

---
+++ spec/robots/guard_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Computer, type: :model do
+    it 'should move 10 cars to store' do
+        require "#{Rails.root}/lib/robots/builder"
+        require "#{Rails.root}/lib/robots/guard"
+        
+        guard = Guard.new
+        builder = Builder.new
+        builder.setGuard (guard)
+        builder.build
+        
+        guard = Guard.new
+        guard.moveToStore
+
+        amount_in_store = StoreStock.all.count
+
+        expect(amount_in_store).to eq(10)
+    end
+  end
+  
